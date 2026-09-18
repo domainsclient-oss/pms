ca5b42926003d5e501c32dc4960f409d3ee933c4
domainsclient-1089 <domainsclient@gmail.com>
2026-09-18 23:14:15 +0530

    Update next.config.ts to specify serverExternalPackages for Firebase and authentication libraries

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["firebase-admin", "google-auth-library", "jwks-rsa", "jose"],
 };
 
 export default nextConfig;
